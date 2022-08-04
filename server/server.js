9d7c5989d12d248f57fc649fb0aaaf678773b695
Odun Freeman <bornduringwar@gmail.com>
2022-08-04 14:37:48 -0400

    server starts and sends json home route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 //inits app
 const app = express();
@@ -10,3 +11,23 @@ dotenv.config({ path: './config/config.env' });
 
 // Body Parser
 app.use(express.json());
+
+//Cors
+app.use(cors());
+
+app.get('/', (req, res, next) => {
+  res.status(200).json({ fuck: 'you' });
+});
+
+const startServer = async () => {
+  const PORT = process.env.PORT;
+  const portString = `${PORT}`.brightYellow;
+
+  const server = app.listen(PORT, () => {
+    console.log(
+      ` Server running in ${process.env.NODE_ENV} , App listening on port ${portString}!`
+        .yellow.bold
+    );
+  });
+};
+startServer();
