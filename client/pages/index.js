c020060d237f076fa5d950ecd57f7d090f28fa34
Odun Freeman <bornduringwar@gmail.com>
2022-08-04 16:09:26 -0400

    test to see if i can pull data from localhost server and render data: SUCCESS

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,19 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const fetchData = async () => {
-    const res = await axios.get('http://localhost:5000');
+  const [items, setItems] = useState([]);
+  const fetchItems = async () => {
+    const res = await axios.get('http://localhost:5000/item/all');
+    setItems(res.data);
     console.log(res.data);
   };
-  fetchData();
 
+  useEffect(() => {
+    fetchItems();
+  }, []);
   return (
     <div>
       <Head>
@@ -19,6 +24,17 @@ export default function Home() {
 
       <main>
         <h1 className="text-bold text-center text-3xl uppercase">aems</h1>
+        {items.map(item => {
+          return (
+            <div className="m-8">
+              <h2>{item.category}</h2>
+              <h2>{item.model}</h2>
+              <h3 className="text-bold text-2xl">
+                ${item.quantity * item.price}
+              </h3>
+            </div>
+          );
+        })}
       </main>
     </div>
   );
