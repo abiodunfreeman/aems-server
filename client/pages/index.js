bb51b56b6f28a53b949e26986aa62fd64360fb9a
Odun Freeman <bornduringwar@gmail.com>
2022-08-19 17:51:07 -0400

    dynamic rendering on / dependent upon whether a user is logged in or not

--- client/pages/index.js
+++ client/pages/index.js
@@ -19,37 +19,44 @@ export default function Home() {
 
       <main className="flex flex-col ">
         <Nav />
-        <div className="max-w-screen-sm self-center border border-black p-8 m-6">
-          <h1 className="text-center mb-3">
-            Welcome to{' '}
-            <span className="font-bold">
-              Atpiv Electronic Management System
-            </span>{' '}
-            (AEMS)
-          </h1>
-          <p>
-            To use AEMS you must first{' '}
-            <Link href="/user/signup">
-              <span className="underline cursor-pointer aptiv-primary xl:text-center">
-                make an account
-              </span>
-            </Link>{' '}
-            or use one of the{' '}
-            <Link href="/user/login">
-              <span className="underline cursor-pointer aptiv-primary xl:text-center">
-                test accounts
-              </span>
-            </Link>
-            <br />
-            <br />
-            Users can view items, and be assigned items by admins. Admins can
-            create categories, and items which fall underneath a specfic
-            category.
-            <br /> <br /> Admins can assign users items up to the number in
-            storage, update those items if they get damaged or lost , and make
-            notes.
-          </p>
-        </div>
+        {!user && (
+          <div className="max-w-screen-sm self-center border border-black p-8 m-6">
+            <h1 className="text-center mb-3">
+              Welcome to{' '}
+              <span className="font-bold">
+                Atpiv Electronic Management System
+              </span>{' '}
+              (AEMS)
+            </h1>
+            <p>
+              To use AEMS you must first{' '}
+              <Link href="/user/signup">
+                <span className="underline cursor-pointer aptiv-primary xl:text-center">
+                  make an account
+                </span>
+              </Link>{' '}
+              or use one of the{' '}
+              <Link href="/user/login">
+                <span className="underline cursor-pointer aptiv-primary xl:text-center">
+                  test accounts
+                </span>
+              </Link>
+              <br />
+              <br />
+              Users can view items, and be assigned items by admins. Admins can
+              create categories, and items which fall underneath a specfic
+              category.
+              <br /> <br /> Admins can assign users items up to the number in
+              storage, update those items if they get damaged or lost , and make
+              notes.
+            </p>
+          </div>
+        )}
+        {user && (
+          <div>
+            <p>logged in now you can do stuff</p>
+          </div>
+        )}
       </main>
     </div>
   );
