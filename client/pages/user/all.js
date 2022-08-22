8829d7f2d647e2e9d9929ab60777b63966306d92
Odun Freeman <bornduringwar@gmail.com>
2022-08-22 10:01:00 -0400

    able to filter users

--- client/pages/user/all.js
+++ client/pages/user/all.js
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
@@ -13,6 +13,32 @@ export default function AllUsers() {
     console.log(user);
   }, [user]);
   const [users, setUsers] = useState([]);
+  const [usersJSX, setUsersJSX] = useState([]);
+  function returnUserJSX(users) {
+    const userJSX = users.map(u => {
+      const url = `/user/${u._id}`;
+      return (
+        <div
+          key={u._id}
+          className="border border-black p-4 flex flex-col items-center gap-3"
+        >
+          <h1 className="cursor-pointer text-bold text-3xl">{u.username}</h1>
+
+          <Link href={url}>
+            <Button variant="outlined">View Profile</Button>
+          </Link>
+          <Button
+            variant="outlined"
+            onClick={() => deleteUser(u._id)}
+            color="error"
+          >
+            Delete User
+          </Button>
+        </div>
+      );
+    });
+    return userJSX;
+  }
   const deleteUser = async id => {
     console.log(user.status);
     if (user.status !== 'admin') {
@@ -27,33 +53,39 @@ export default function AllUsers() {
   };
   const getUsers = async () => {
     const res = await axios.get('http://localhost:5000/user/all');
-    const userJSX = res.data.map(u => {
-      const url = `/user/${u._id}`;
-      return (
-        <div key={u._id} className="">
-          <Link href={url}>
-            <h1 className="cursor-pointer text-bold text-3xl">{u.username}</h1>
-          </Link>
 
-          <Button variant="outlined" onClick={() => deleteUser(u._id)}>
-            Delete User
-          </Button>
-        </div>
-      );
-    });
-    setUsers(userJSX);
-    console.log(res.data);
+    setUsersJSX(returnUserJSX(res.data));
+    setUsers(res.data);
   };
 
   useEffect(() => {
     getUsers();
   }, []);
+  useEffect(() => {
+    console.log(users);
+  }, [users]);
+
+  function handleFilterUsers(e) {
+    const filteredUsers = users.filter(user =>
+      user.username.includes(e.target.value)
+    );
+    setUsersJSX(returnUserJSX(filteredUsers));
+  }
   return (
-    <div className="">
+    <div className="min-h-screen flex flex-col">
       <Nav />
       <h1>{errMsg}</h1>
-      <h1>View All Users</h1>
-      {users}
+      <TextField
+        className="self-center mb-4"
+        id="filter-user-input"
+        variant="standard"
+        placeholder="enter name"
+        label="filter users"
+        onChange={handleFilterUsers}
+      />
+      <div className=" flex gap-4 flex-wrap p-4 self-center  justify-center">
+        {usersJSX}
+      </div>
     </div>
   );
 }
