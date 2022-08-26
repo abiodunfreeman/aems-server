75a6f8dc58f20e851f235ef4a5b30fad30bc739d
Odun Freeman <bornduringwar@gmail.com>
2022-08-25 20:22:23 -0400

    update header Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -33,7 +33,10 @@ app.use(
 );
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://aems-8zcvzcqyq-abiodunfreeman.vercel.app/'
+  );
   res.header('Access-Control-Allow-Credentials', true);
   res.header(
     'Access-Control-Allow-Headers',
